c533d35debcec1fd7678d219597563f180fcf6cd
acblackburn <alexcblackburn98@gmail.com>
2020-08-11 11:12:49 +0100

    Cleaned up folder structure for css and js files and renamed script.js -> main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Toggle between showing and hiding mobile navigation menu
 function toggleNavMenu() {
-    var brand_bg_color = document
+    var brand_bg_color = document.header.h
     if (brand_bg_color.style.backgroundColor === "#f1f1f1") {
         brand_bg_color.style.backgroundColor = "#111"
     } else {
