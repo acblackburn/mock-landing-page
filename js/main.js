48c1e53f4681ec823598fca4191a039bd0ba38a7
acblackburn <alexcblackburn98@gmail.com>
2020-08-16 19:50:47 +0100

    Writing toggleNav functionality into main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
 // Toggle between showing and hiding mobile navigation menu
-function toggleNavMenu() {
-    var brand_bg_color = document.header.h
-    if (brand_bg_color.style.backgroundColor === "#f1f1f1") {
-        brand_bg_color.style.backgroundColor = "#111"
-    } else {
-        brand_bg_color.style.backgroundColor = "#f1f1f1"
-    }
+let toggleNavStatus = false;
+
+let toggleNav = function() {
+  let getBrand = document.querySelector("header-brand");
 }
